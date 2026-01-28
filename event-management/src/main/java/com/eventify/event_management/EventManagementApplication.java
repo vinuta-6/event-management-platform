996c8fdf19476569625d92470a5ab90d4e547e0d
Vinuta Jainapur <jainapurvinuta@gmail.com>
2026-01-28 11:00:48 +0530

    Initial backend setup for event management platform

---
+++ event-management/src/main/java/com/eventify/event_management/EventManagementApplication.java
@@ -0,0 +1,14 @@
+
+package com.eventify.event_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementApplication.class, args);
+	}
+
+}

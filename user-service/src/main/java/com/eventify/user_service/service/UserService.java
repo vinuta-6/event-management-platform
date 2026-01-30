dcd97f294d2e2e3420e6d6b99808aa350d2fbd0d
Jasmeet Singh <jasmeetsinghkhokhar@gmail.com>
2026-01-31 00:27:10 +0530

    cleanup

--- user-service/src/main/java/com/eventify/user_service/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.eventify.user_service.service;
-
-import com.eventify.user_service.entity.User;
-
-public interface UserService {
-    User register(User user);
-
-    User getByEmail(String email);
-}
